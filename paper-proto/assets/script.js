b39e329657d60a18410c75cb435e15c9920caf50
Gabe Air <gabriel.ruttner@gmail.com>
2016-09-23 11:16:56 -0400

    csv

--- paper-proto/assets/script.js
+++ paper-proto/assets/script.js
@@ -33,7 +33,7 @@ $(function(){
   $("#ex3").slider({min  : 0, max  : 10, value: 7, tooltip_position:'bottom'});
 
   //live populating
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < 0; i++) {
     $(transcriptEntry("1:0"+i+" pm", "assets/images/gabriel_ruttner.jpg", entries[i].q, entries[i].a))
       .appendTo($("#liveTimeline"));
       current++;
